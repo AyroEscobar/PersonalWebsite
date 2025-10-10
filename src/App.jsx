0a9f5cc7971cb1b9ce4be18e57d99abf096c955e
Ayro Escobar <ayro.escobar@gmail.com>
2025-10-10 12:56:03 -0500

    fixing tailwindcss version to version 3 so it works on everything

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,9 @@ import { Outlet } from 'react-router-dom'
 
 function App() {
   return (
-    <div class="min-h-screen w-full bg-red-800 flex flex-col ">
+    <div className="min-h-screen w-full flex flex-col ">
+    <div className="bg-red-500 text-yellow-300 p-8">TEST TAILWIND</div>
       
-   
       <Navbar/>
       <Outlet/>
     </div>
--- src/components/pages/about.jsx
+++ src/components/pages/about.jsx
@@ -8,13 +8,13 @@ import pfp from '../../assets/pfp.JPG'
 function About() {
   return (
     <div className="flex justify-center p-8">
-      <div className="flex items-center gap-8 bg-white/10 backdrop-blur-sm rounded-2xl p-8 max-w-6xl">
+      <div className="flex items-center gap-8 bg-white/10 backdrop-blur-sm rounded-2xl p-8 max-w-6xl pt-[20vh] mx-[15%] ">
         {/* Left side - Image */}
         <div className="flex-shrink-0">
           <img 
             src={pfp} 
             alt='Profile Picture' 
-            className="h-[20rem] w-[20rem] rounded-full object-cover border-8 border-yellow-400 object-right"
+            className="h-[20rem] w-[20rem] rounded-full object-cover border-8 border-yellow-400 object-right "
           />
         </div>
         
